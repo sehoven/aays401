bf12fa2c69d2bb772c86d1db2e5e00a073da1a3f
Etienne Asselin <erasseli@ualberta.ca>
2017-09-29 00:58:20 -0600

    Added neighbourhood JSON objects (count = 388), and modified server to load them and respond to a query with them.

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// Load local neighbourhood data synchronously
+console.log("Loading data into memory...");
+var neighborhood_data = require('./data/neighborhoods.json');
+var neighborhoods = fs.readFileSync('./data/neighborhoods.json', 'utf8');
+neighborhoods = JSON.parse(neighborhoods);
+neighborhoods = neighborhoods.neighborhoods;
+console.log("Ready. Server Started.");
 
 app.use(bodyParser.json());
-
-dummyLocations = [];
-var Belvedere = {
-  name: "Belvedere",
-  lat: 53.5910582,
-  lng: -113.4393493
-};
-var Manning = {
-  name: "Manning",
-  lat: 53.635534,
-  lng: -113.3957664
-};
-var MacEwan = {
-  name: "MacEwan",
-  lat: 53.5470543,
-  lng: -113.508819
-};
-dummyLocations.push(Belvedere);
-dummyLocations.push(Manning);
-dummyLocations.push(MacEwan);
 
 // Prepare Access-Control
 app.use(function (req, res, next) {
@@ -35,19 +24,23 @@ app.use(function (req, res, next) {
 });
 
 // Handle requests to "/nearby"
-// Returns all known locations that are within rad from point (lat, lng)
+// Returns all known neighborhoods that are within rad from point (lat, lng)
 app.get('/nearby', function(req, res) {
   console.log("Location request handler invoked");
+  //Should add some kind of type validation here.. and everywhere.
   if (!req.query.lat || !req.query.lng || !req.query.rad)
     return res.sendStatus(400);
 
   let resBody = [];
-  for (var i = 0; i < dummyLocations.length; i++) {
-    var a = req.query.lat - dummyLocations[i].lat;
-    var b = req.query.lng - dummyLocations[i].lng;
+  for (var i = 0; i < neighborhoods.length; i++) {
+    var c = parseFloat(req.query.rad) + neighborhoods[i].radius;
+    var a = parseFloat(req.query.lat) - neighborhoods[i].center.lat;
+    var b = parseFloat(req.query.lng) - neighborhoods[i].center.lng;
 
-    if (Math.sqrt( a*a + b*b ) < req.query.rad){
-        resBody.push(dummyLocations[i]);
+    console.log(a, b, c);
+
+    if (Math.sqrt( a*a + b*b ) < c){
+        resBody.push(neighborhoods[i]);
     }
   }
 
