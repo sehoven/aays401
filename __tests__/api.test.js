19b7cb99d345148d5ff358441c05b71586322d6c
ismailmare <imare@ualberta.ca>
2017-10-16 19:09:45 -0600

    File added to test API. This test file tests the search functionality and the polygon count using chai and jest. To run tests, simply type [npm test]

---
+++ __tests__/api.test.js
@@ -0,0 +1,111 @@
+
+//Dependencies
+const fs = require('fs');
+var expect = require('chai').expect;
+var request = require('request');
+
+describe('Main Page Testing', function() {
+    //Check status of main page
+    it('Main page status is 200', function(done) {
+    
+        var term = "RUTHERFORD";
+        request('http://localhost:8080' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+
+
+describe('Search Testing', function() {
+    //Search call should return status of 200
+    it('Main page search returns 200',function(done) {
+
+        var term = "RUTHERFORD";
+        request(` http://localhost:3000/locations?name=${term}`,function(error,response,body){
+            expect(response.statusCode).to.equal(200);
+            done();
+        }); 
+    });
+
+
+    //Search should return data for this test
+    it('Main page search returns correct data according to search term',function(done) {
+    
+        var term = "RUTHERFORD";
+        request(` http://localhost:3000/locations?name=${term}`,function(error,response,body){
+            var obj = JSON.parse(body);
+            expect(obj[0].name).to.equal(term);
+            done();
+        });
+    });
+
+
+    //Search should return nothing for this test
+    it('Main page search returns nothing based on search term',function(done) {
+    
+        var term = "lkhfgjkhsafjahsd";
+        request(` http://localhost:3000/locations?name=${term}`,function(error,response,body){
+            var obj = JSON.parse(body);
+            expect(obj).to.be.empty;
+            done();
+        });
+    });
+
+});
+
+
+describe('Polygon Count Testing', function() {
+    //Poly count should return status of 400
+    it('Poly count should return status of 400',function(done) {
+    
+
+        //Variables for testing
+        var neighborhoods = fs.readFileSync('./data/neighborhoods.json', 'utf8');
+        neighborhoods = JSON.parse(neighborhoods);
+        neighborhoods = neighborhoods.neighborhoods[0];
+        var center = 53.5727667926;
+        var radius = 1;
+
+        let body = JSON.stringify({ "poly" : neighborhoods.points});
+        request( ` http://localhost:3000/addressCount` ,
+                { "method": 'POST',
+                "body": body,
+                "headers": {  'Content-Type': 'application/json',
+                'Content-Length': new Buffer(body).length }},function(error,response,body){
+
+            expect(response.statusCode).to.equal(400);
+            done();
+        });
+    });
+
+
+    //Poly count should return status of 200 along with data
+    it('Poly count should return status of 200 along with data',function(done) {
+    
+        //Variables for testing
+        var neighborhoods = fs.readFileSync('./data/neighborhoods.json', 'utf8');
+        neighborhoods = JSON.parse(neighborhoods);
+        neighborhoods = neighborhoods.neighborhoods[5];
+        var center = 53.5727667926;
+        var radius = 1;
+
+        let body = JSON.stringify({ "poly" : neighborhoods.points,"center":center,"radius":radius});
+        request( ` http://localhost:3000/addressCount` ,
+                { "method": 'POST',
+                "body": body,
+                "headers": {  'Content-Type': 'application/json',
+                'Content-Length': new Buffer(body).length }},function(error,response,body){
+
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.not.be.equal({});
+        
+            //To print unit counts of neighbourhood
+            //console.log("\n");
+            //console.log(neighborhoods.name+"\n");
+            //console.log(body);
+            done();
+        });
+    });
+});
+
