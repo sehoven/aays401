1c98e6e0608d73dff6f08956d0d540e9bac8a85c
lazyhow <wuhao1994@hotmail.com>
2017-11-08 17:31:46 -0700

    Commit before pull

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ export class Index extends Component {
     this.state = {
       mapLoaded: false,
       map: null,
-      maps: null
+      maps: null,
+      currentPanel: "search"
     }
     this.setMapRef = this.setMapRef.bind(this);
   }
@@ -24,29 +25,73 @@ export class Index extends Component {
       maps: maps
     });
   }
+  swapState(toggle){
+    this.setState({ currentPanel : toggle })  ;
+  }
 
   render(){
+    var topButtonStyle = {};
+    var bottomButtonStyle = {};
+    if (this.state.currentPanel == "search"){
+      topButtonStyle = {
+        borderTop: "1px black solid",
+        borderRight: "1px black solid",
+        borderBottom: "1px black solid",
+        backgroundColor: "white"
+      }
+      bottomButtonStyle = {
+        borderLeft: "1px black solid",
+        backgroundColor: "gray"
+      }
+    } else {
+      topButtonStyle = {
+        borderLeft: "1px black solid",
+        backgroundColor: "gray"
+      }
+        bottomButtonStyle = {
+        borderTop: "1px black solid",
+        borderRight: "1px black solid",
+        borderBottom: "1px black solid",
+        backgroundColor: "white"
+      }
+    }
     return (
       <div className="fullScreen">
-        <div className="container">
-          <Tabs className="container" ref = {instance => {this.tabs = instance}}>
-            <div name="search">
+          <div id = "leftContainer">
+            { (this.state.currentPanel == "search") &&
               <NavPanel
                 index={this.state}
                 overlayRef={this.overlay}
                 tabsRef={this.tabs}
               />
-            </div>
-            <div name="polygon">
+            }
+            { (this.state.currentPanel == "draw") &&
               <Overlay
                 ref={instance => {this.overlay = instance}}
                 map={this.state.map}
                 maps={this.state.maps}
               />
+            }
+            <div id = "tabButtons">
+              <div
+                className = "tabButton"
+                id = "topTabButton"
+                onClick = {() => {this.swapState("search")}}
+                style = { topButtonStyle }
+              >
+                Search
+              </div>
+              <div
+                className = "tabButton"
+                id = "bottomTabButton"
+                onClick = {() => {this.swapState("draw")}}
+                style = { bottomButtonStyle }
+              >
+                Draw
+              </div>
             </div>
-          </Tabs>
-          <Map setMapRef={this.setMapRef} />
 	      </div>
+        <Map setMapRef={this.setMapRef} />
       </div>
     )
   }
