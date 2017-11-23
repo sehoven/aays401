5c2a1a0b029abeb9ea6464b9ef7eeb44e520e63e
ismailmare <imare@ualberta.ca>
2017-11-22 18:47:31 -0700

    Authentication setup for database / server API calls

--- src/components/HTTPService.js
+++ src/components/HTTPService.js
@@ -26,3 +26,29 @@ export function countPolyResidences(polyData) {
         return res.json();
     });
 }
+
+export function login(info) {
+  let body = JSON.stringify({ "username" : info.username,"password":info.password});
+  return fetch( ` http://localhost:3000/login` ,
+                { "method": 'POST',
+                  "body": body,
+                  "headers": {  'Content-Type': 'application/json',
+                  'Content-Length': new Buffer(body).length }})
+    .then(function(res) {
+      return res.json();
+  });
+
+}
+
+export function signup(info) {
+  let body = JSON.stringify({ "username" : info.username,"password":info.password,"email":info.email});
+  return fetch( ` http://localhost:3000/signup` ,
+                { "method": 'POST',
+                    "body": body,
+                    "headers": {  'Content-Type': 'application/json',
+                    'Content-Length': new Buffer(body).length }})
+    .then(function(res) {
+      return res.json();
+  });
+
+}
