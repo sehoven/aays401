ad811ae413e488a77444cc687f1e31d37cbae5d6
sehoven <shoven@ualberta.ca>
2017-11-28 23:12:59 -0700

    Default authenticated to true until the sign up button is complete.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,7 +9,7 @@ export default class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: true
     }
 
     this.setAuthenticated = this.setAuthenticated.bind(this);
